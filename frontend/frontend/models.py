51c25ff4c6f036309cb0120778c35911bc95d25b
HarishGowda-Git <93182559+HarishGowda-Git@users.noreply.github.com>
2025-05-30 07:18:08 +0530

    Delete frontend directory

--- frontend/frontend/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Location(models.Model):
-    name = models.CharField(max_length=255)
-    latitude = models.FloatField(48.8584)
-    longitude = models.FloatField(2.2945)
-
-    def __str__(self):
-        return self.name
